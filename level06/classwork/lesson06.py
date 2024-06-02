9f25dc2041ebbd4879d8140a839fd14a32d06d00
lizi2288 <Lizimacharashvili845@gmail.com>
2024-06-02 10:51:03 +0400

    GOA

--- level06/classwork/lesson06.py
+++ level06/classwork/lesson06.py
@@ -1,5 +1,14 @@
+
 name = "data"
 surname = "diasamidze"
 age = 100
 
 print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) ) 
+
+
+
+name = "data
+surname = diasamidze"
+age = 100
+
+print("My name is" + " " + name + "my surname is" + " " + surname + "my age is" + " " + age)
---
+++ level06/homework/homework06.py
@@ -0,0 +1,16 @@
+name = "data"
+surname = "diasamidze"
+age = 100
+text = "I am programmer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+
+
+name = "lizi"
+surname = "macharashvili"
+age = 14
+text = "I am programer"
+
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
