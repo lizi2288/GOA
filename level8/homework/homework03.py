1f360bfcb49d515815a37de1a9cb805181a6c85f
lizi2288 <Lizimacharashvili845@gmail.com>
2024-06-13 00:20:48 +0400

    commit

---
+++ level8/homework/homework03.py
@@ -0,0 +1,4 @@
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+print("momxmareblis saxelia" + name + ", momxmareblis gvaria" + lastname + ", momxmareblis asakia" + age + ".")
