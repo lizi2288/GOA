3d56898cd2f42ce9e297108d53b9f09c72fab12d
lizi2288 <Lizimacharashvili845@gmail.com>
2024-07-15 18:13:51 +0400

    GOA

---
+++ level18/classwork/lesson18.py
@@ -0,0 +1,18 @@
+for i in range(30):
+    print("hello world")
+
+    for i in range(1,10):
+        print(i)
+
+        for i in range(10,20,2):
+            print(i)
+
+            
+
+
+
+
+
+
+
+    
---
+++ level18/homework/homework18.py
@@ -0,0 +1,21 @@
+#8
+for i in reversed(range(0,101)):
+    print(i)
+
+
+    #9
+    for i in reversed(range(100,0)):
+        print(i)
+
+        #4
+        for i in reversed(range(1000,0,100)):
+            print(i)
+
+
+            #6
+            while number !=100:
+                print("hello world")
+
+
+
+
